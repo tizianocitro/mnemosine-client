fd6c56f89ab2c8e0a34a73b22a0f465f08bca3a2
Tiziano <tizianocitro98@gmail.com>
2021-01-28 12:28:06 +0100

    Deployment settings

--- src/app/consts/consts.ts
+++ src/app/consts/consts.ts
@@ -1,7 +1,9 @@
-export const baseUrl = "http://localhost:8080/mnemosine/";
+export const baseUrl = "https://mnemosine.azurewebsites.net/";
+export const localBaseUrl = "http://localhost:8080/mnemosine/";
+export const homeUrl = "https://mnemosinewepapp.azurewebsites.net/";
+export const localHomeUrl = "http://localhost:4200/";
+
 export const clientId = "d6598c30-28ee-4b87-91e5-4644e58b9042";
 export const secret = ".sBXKCY5-.S2VtFn_HgbBZzHt9J99.YzuL";
 export const subscriptionId = "deebd589-67f5-4936-be66-5759887750f5";
 export const tenantId = "c30767db-3dda-4dd4-8a4d-097d22cb99d3";
-
-export const homeUrl = "http://localhost:4200/";
